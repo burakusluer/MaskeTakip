522f47e8d047da92baf8174ab9899b0d61f1ae71
Burak Usluer <50269070+burakusluer@users.noreply.github.com>
2024-02-05 22:34:23 +0300

    after first break first class Vatandas added to project "Workaround" second project added to solution called "Entities" with class Person

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,9 +1,21 @@
-﻿string mesaj = "Merhaba";
-double tutar = 10000;//db den gelicek
-int sayi = 100;
-bool girisYapmisMi = false;
+﻿//Degiskenler();
+Vatandas vatandas1 = new Vatandas();
 
-string ad = "Engin";
-string soyad = "Demiroğ";
-int dogumYili = 1985;
-long tcNo = 12345678910;
+void Degiskenler()
+{
+    string mesaj = "Merhaba";
+    double tutar = 10000;//db den gelicek
+    int sayi = 100;
+    bool girisYapmisMi = false;
+}
+
+public class Vatandas
+{
+    public string Ad { get; set; }
+    public string Soyad { get; set; }
+    public int DogumYili { get; set; }
+    public long TcNo { get; set; }
+    //kelimelerin ilk harfleri büyük ise PascalCase
+    //snake_case
+    //camelCase
+}
