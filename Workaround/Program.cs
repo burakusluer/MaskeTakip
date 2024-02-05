276636f5c83c5865502da48397480789c3bef9bf
Burak Usluer <50269070+burakusluer@users.noreply.github.com>
2024-02-05 22:10:26 +0300

    before starting classes and oop

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,1 +1,9 @@
-﻿
+﻿string mesaj = "Merhaba";
+double tutar = 10000;//db den gelicek
+int sayi = 100;
+bool girisYapmisMi = false;
+
+string ad = "Engin";
+string soyad = "Demiroğ";
+int dogumYili = 1985;
+long tcNo = 12345678910;
