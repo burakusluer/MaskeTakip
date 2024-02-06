bcf5d75c233974079bc71b087e316c5d175a686a
Burak Usluer <50269070+burakusluer@users.noreply.github.com>
2024-02-06 16:21:08 +0300

    List<type> generic types and value & reference type variables sampled in this lesson

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,13 +1,44 @@
 ﻿//Degiskenler();
 //Vatandas vatandas1 = new Vatandas();
 
-SelamVer("Engin");
-SelamVer("Ahmet");
-SelamVer("Eyşe");
-SelamVer();
+//SelamVer("Engin");
+//SelamVer("Ahmet");
+//SelamVer("Eyşe");
+//SelamVer();
 
-int sonuc = Topla(6,58);
+//int sonuc = Topla(6,58);
 
+
+//string ogr1 = "Engin";
+//string ogr2 = "Kerem";
+//string ogr3 = "Berkay";
+//string[] ogrArray = {ogr1,ogr2, ogr3};
+
+//foreach (string ogr in ogrArray)
+//{
+//    Console.WriteLine(ogr);
+//}
+
+//string[] sehirler1 = { "İzmir", "Ankara", "İstanbul" };
+//string[] sehirler2 = { "İzmir2", "Ankara2", "İstanbul2" };
+//sehirler1 = sehirler2;
+//sehirler1[0] = "Smyrna";
+//Console.WriteLine(sehirler2[0]);//reference type olduğu için smyrna yazar sehirler1 ve
+//ve sehirler2 bellekte aynı adresi refere ediyor
+
+//int sayi1 = 10;
+//int sayi2 = 20;
+//sayi1 = sayi2;
+//sayi2 = 300;
+//Console.WriteLine(sayi1);//10 value type olduğu için 
+
+List<string> listSehirler = new List<string> { "Ankara", "İstanbul", "Paris" };
+listSehirler.Add("new Ankara");
+
+foreach (string str in listSehirler)
+{
+    Console.WriteLine(str);
+}
 
 void SelamVer(string isim="noname")//static main method olmadığı için yazmadım
 {
