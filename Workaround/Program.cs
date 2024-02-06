c39d822a03efc67f74f1205f19f5db6a7176204c
Burak Usluer <50269070+burakusluer@users.noreply.github.com>
2024-02-06 15:29:53 +0300

    Business project added

--- Workaround/Program.cs
+++ Workaround/Program.cs
@@ -1,5 +1,25 @@
 ﻿//Degiskenler();
-Vatandas vatandas1 = new Vatandas();
+//Vatandas vatandas1 = new Vatandas();
+
+SelamVer("Engin");
+SelamVer("Ahmet");
+SelamVer("Eyşe");
+SelamVer();
+
+int sonuc = Topla(6,58);
+
+
+void SelamVer(string isim="noname")//static main method olmadığı için yazmadım
+{
+    Console.WriteLine($"Merhaba {isim}");
+}
+
+int Topla(int sayi1=5,int sayi2 = 20)
+{
+    int sonuc=sayi1 + sayi2;
+    Console.WriteLine($"Toplam : {sonuc}");
+    return sonuc;
+}
 
 void Degiskenler()
 {
